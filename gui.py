4367f08213de463c2a039cd0cd7bf0f366da55e6
zihadev <34001897+zihadev@users.noreply.github.com>
2024-07-18 18:23:34 +0200

    change from PySimpleGui to FreeSimpleGui

--- gui.py
+++ gui.py
@@ -1,20 +1,20 @@
 import functions
-import PySimpleGUI as sg
+import FreeSimpleGUI as fsg
 import time
 
-sg.theme("Black")
+fsg.theme("Black")
 
-clock = sg.Text('', key='clock')
-label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add", size=10)
-list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+clock = fsg.Text('', key='clock')
+label = fsg.Text("Type in a to-do")
+input_box = fsg.InputText(tooltip="Enter todo", key="todo")
+add_button = fsg.Button("Add", size=10)
+list_box = fsg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
-edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
-exit_button = sg.Button("Exit")
+edit_button = fsg.Button("Edit")
+complete_button = fsg.Button("Complete")
+exit_button = fsg.Button("Exit")
 
-window = sg.Window('My To-Do App',
+window = fsg.Window('My To-Do App',
                    layout=[[clock],
                            [label],
                            [input_box, add_button],
@@ -25,41 +25,44 @@ window = sg.Window('My To-Do App',
 while True:
     event, values = window.read(timeout=200)
     window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
-    match event:
-        case "Add":
+    if event == "Add":
+        todos = functions.get_todos()
+        new_todo = values['todo'] + "\n"
+        todos.append(new_todo)
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+
+    elif event == "Edit":
+        try:
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
             todos = functions.get_todos()
-            new_todo = values['todo'] + "\n"
-            todos.append(new_todo)
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        except IndexError:
+            fsg.popup("Please select an item first.", font=("Helvetica", 20))
 
-        case "Edit":
-            try:
-                todo_to_edit = values['todos'][0]
-                new_todo = values['todo']
+    elif event == "Complete":
+        try:
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        except IndexError:
+            fsg.popup("Please select an item first.", font=("Helvetica", 20))
 
-                todos = functions.get_todos()
-                index = todos.index(todo_to_edit)
-                todos[index] = new_todo
-                functions.write_todos(todos)
-                window['todos'].update(values=todos)
-            except IndexError:
-                sg.popup("Please select an item first.", font=("Helvetica", 20))
-        case "Complete":
-            try:
-                todo_to_complete = values['todos'][0]
-                todos = functions.get_todos()
-                todos.remove(todo_to_complete)
-                functions.write_todos(todos)
-                window['todos'].update(values=todos)
-                window['todo'].update(value='')
-            except IndexError:
-                sg.popup("Please select an item first.", font=("Helvetica", 20))
-        case "Exit":
-            break
-        case 'todos':
-            window['todo'].update(value=values['todos'][0])
-        case sg.WIN_CLOSED:
-            break
+    elif event == "Exit":
+        break
+
+    elif event == 'todos':
+        window['todo'].update(value=values['todos'][0])
+
+    elif event == fsg.WIN_CLOSED:
+        break
 
 window.close()
